ce1fd421e79a7e7f63e5463e4648409ebbc165d5
藤井邦生 <kunio.f3@gmail.com>
2015-06-14 18:52:46 +0900

    れいやのHTMLを一部取り込み

--- AGEs_project/AGEs/urls.py
+++ AGEs_project/AGEs/urls.py
@@ -3,6 +3,7 @@ from AGEs.views import home_views, person_views, picture_views, post_views
 
 urlpatterns = patterns('',
         url(r'^$', home_views.home, name='home'),
+        url(r'^about/$', home_views.about, name='about'),
         url(r'^person/$', person_views.person, name='person'),
         url(r'^add_person/$', post_views.add_person, name='add_person'),
         url(r'^add_picture/(?P<slug>[\w\-]+)$', post_views.add_picture, name='add_picture'),
--- AGEs_project/AGEs/views/home_views.py
+++ AGEs_project/AGEs/views/home_views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 # home画面遷移時のView
 def home(request):
     #テンプレートエンジンにコンテキストとしてDictionaryを用意する
-    context_dict = {'boldmessage' : "Welcome to AGEs!!!"}
+    #context_dict = {'boldmessage' : "Welcome to AGEs!!!"}
+    context_dict = {}
     # Client側にレンダリングしたresponseを送る
     return render(request, 'AGEs/home.html', context_dict)
+
+# about画面遷移時のView
+def about(request):
+    #テンプレートエンジンにコンテキストとしてDictionaryを用意する
+    #context_dict = {'boldmessage' : "Welcome to AGEs!!!"}
+    context_dict = {}
+    # Client側にレンダリングしたresponseを送る
+    return render(request, 'AGEs/whatsages.html', context_dict)
