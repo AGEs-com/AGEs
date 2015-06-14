0052277cdacd46cc2e20d8f9954d09beb4f0dca2
藤井邦生 <kunio.f3@gmail.com>
2015-06-14 19:29:23 +0900

    Upload.htmlを取り込み、他のファイルを編集

---
+++ AGEs_project/AGEs/migrations/0001_initial.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('category_name', models.CharField(max_length=64, unique=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Item',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('item_name', models.CharField(max_length=128)),
+                ('item_id', models.CharField(max_length=128)),
+                ('num_of_pictures', models.IntegerField(default=0)),
+                ('slug', models.SlugField(unique=True)),
+                ('category', models.ForeignKey(to='AGEs.Category')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Picture',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('picture_name', models.CharField(max_length=128)),
+                ('register_date', models.DateField(default=datetime.datetime.now)),
+                ('age', models.IntegerField(default=0)),
+                ('description', models.CharField(max_length=200)),
+                ('image', models.ImageField(upload_to='picture', blank=True)),
+                ('item', models.ForeignKey(to='AGEs.Item')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
